55c43d8914eedbc3e43c69c8d5fae07a968ed678
Jonathan <j.weckerle@jpberlin.de>
2019-12-22 19:17:58 +0100

    Fix selectors

--- js/src/wimmelpicture.js
+++ js/src/wimmelpicture.js
@@ -1,58 +1,60 @@
 document.addEventListener("DOMContentLoaded", initWimmelPicture);
 
 function initWimmelPicture() {
-    const outlinesSvg = document.querySelector(".wimmel__outlines");
-    outlinesSvg.addEventListener("click", handleOutlineClick);
-    outlinesSvg.addEventListener("keydown", handleKeydown);
+  const outlinesSvg = document.querySelector(".wimmel__outlines");
+  outlinesSvg.addEventListener("click", handleOutlineClick);
+  outlinesSvg.addEventListener("keydown", handleKeydown);
 
-    for (const outline of document.querySelectorAll(".wimmel__outlines > g")) {
-        outline.tabIndex = "0";
-        outline.addEventListener("focus", e => {
-            clearAllOutlines();
-            e.currentTarget.classList.add("focus");
-        });
-        outline.addEventListener("focusout", e =>
-            e.currentTarget.classList.remove("focus")
-        );
-    }
+  for (const outline of document.querySelectorAll(".wimmel__outlines > g")) {
+    outline.tabIndex = "0";
+    outline.addEventListener("focus", e => {
+      clearAllOutlines();
+      e.currentTarget.classList.add("focus");
+    });
+    outline.addEventListener("focusout", e =>
+      e.currentTarget.classList.remove("focus")
+    );
+  }
 }
 
 function handleOutlineClick(e) {
-    clearAllOutlines();
-    const clickedElement = e.target;
-    const clickedOutline = clickedElement.closest(".comic__outlines > g");
-    if (!clickedOutline) {
-        return;
-    }
+  clearAllOutlines();
+  const clickedElement = e.target;
+  const clickedOutline = clickedElement.closest(".wimmel__outlines > g");
+  if (!clickedOutline) {
+    return;
+  }
 
-    clickedOutline.classList.add("comic__outline--clicked");
-    clickedOutline.classList.remove("focus");
+  clickedOutline.classList.add("wimmel__outline--clicked");
+  clickedOutline.classList.remove("focus");
 }
 
 function handleKeydown(e) {
-    console.log(e);
-    if (e.keyCode === 27) {
-        // Escape
-        clearAllOutlines();
-        return;
-    }
-    if (![13, 32].includes(e.keyCode)) {
-        // Space or Enter
-        return;
-    }
-
-    const focusedElement = e.target;
-    const focusedOutline = focusedElement.closest(".comic__outlines > g");
-    if (!focusedOutline) {
-        return;
-    }
+  console.log(e);
+  if (e.keyCode === 27) {
+    // Escape
     clearAllOutlines();
-    focusedOutline.classList.add("comic__outline--clicked");
-    e.preventDefault();
+    return;
+  }
+  if (![13, 32].includes(e.keyCode)) {
+    // Space or Enter
+    return;
+  }
+
+  const focusedElement = e.target;
+  const focusedOutline = focusedElement.closest(".wimmel__outlines > g");
+  if (!focusedOutline) {
+    return;
+  }
+  clearAllOutlines();
+  focusedOutline.classList.add("wimmel__outline--clicked");
+  e.preventDefault();
 }
 
 function clearAllOutlines() {
-    for (const outline of document.querySelectorAll(".comic__outline--clicked")) {
-        outline.classList.remove("comic__outline--clicked");
-    }
+  for (const outline of document.querySelectorAll(
+    ".wimmel__outline--clicked"
+  )) {
+    outline.classList.remove("wimmel__outline--clicked");
+  }
 }
